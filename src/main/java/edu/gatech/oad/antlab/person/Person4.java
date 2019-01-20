b99845dccf59a372698bed48aa7fdd865d003d6f
renawrli <renawrli@gmail.com>
2019-01-20 11:20:53 -0500

    Implemented shift string method

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      	String res = "";
+	for (Char c: input) {
+		res+=(Char) (((int) c)+1);		
+	}
+	return res;
     }
     
     /**
